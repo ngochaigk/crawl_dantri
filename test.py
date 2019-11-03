4fc29b5b57c730e1858c1601887b5e2bc7504794
Hai Nguyen <56548651+ngochaigk@users.noreply.github.com>
2019-11-04 01:02:29 +0700

    Create test.py

---
+++ test.py
@@ -0,0 +1,13 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+url = 'https://zingmp3.vn/album/Top-100-Bai-Hat-Nhac-Tre-Hay-Nhat-Various-Artists/ZWZB969E.html'
+response = requests.get(url)
+soup = BeautifulSoup(response.content, 'html.parser')
+post_elements=soup.find(id='root')
+all_post=list(post_elements.children)
+
+
+with open('all_post.json', 'wt', encoding='utf-8') as f:
+   f.write(json.dumps(all_post, ensure_ascii=False))
